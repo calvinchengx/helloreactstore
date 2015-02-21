21e299c7b28cadb4b94d8ac518b61f3739bfb290
Calvin Cheng <linchuan.cheng@gmail.com>
2015-02-22 00:42:06 +0800

    Implement select dropdown

--- src/js/components/FluxProduct.jsx
+++ src/js/components/FluxProduct.jsx
@@ -2,13 +2,26 @@
 
 var React = require('react');
 
+var FluxCartActions = require('../actions/FluxCartActions');
+
 var FluxProduct = React.createClass({
+  selectVariant: function(event) {
+    FluxCartActions.selectProduct(event.target.value);
+  },
   render: function() {
     return (
       <div className="flux-product">
         <h1 className="name">{this.props.product.name}</h1>
         <p className="description">{this.props.product.description}</p>
         <p className="price">Price: ${this.props.selected.price}</p>
+        <select onChange={this.selectVariant}>
+          {this.props.product.variants.map(function(variant, index){
+              return (
+                <option key={index} value={index}>{variant.type}</option> 
+              )  
+            }
+          )}
+        </select>
       </div>
     );
   }
