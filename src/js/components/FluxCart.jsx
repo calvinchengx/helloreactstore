a45a80fcc74d73d8f93fa8fe0f80547c37f03269
Calvin Cheng <linchuan.cheng@gmail.com>
2015-02-23 15:09:32 +0800

    Getting a strong grasp of FluxCart component and how to pass state value as props value

--- src/js/components/FluxCart.jsx
+++ src/js/components/FluxCart.jsx
@@ -6,6 +6,7 @@ var FluxCart = React.createClass({
 
   render: function() {
     console.log('FluxCart rendering');
+    var self = this;
     var products = this.props.products;
     return (
       <div className="flux-cart">
@@ -14,12 +15,15 @@ var FluxCart = React.createClass({
           {Object.keys(products).map(function(product) {
                 return (
                     <li>
-                        {products[product].name}
+                      <h1 className="name">{products[product].name}</h1> 
+                      <p className="type">{products[product].type} x {products[product].quantity}</p>
+                      <p className="price">${(products[product].price * products[product].quantity).toFixed(2)}</p>
                     </li> 
                 ) 
             })
           }
         </ul>
+        <span className="total">Total: ${self.props.total}</span>
       </div>
     );
   }
